f389f6d1ddd91dbe3d7a7e0ad0712d23ead0613c
Kyle Gadd <kyle.gadd@gmail.com>
2016-10-07 15:03:38 -0800

    Update Blog.php

--- src/Blog.php
+++ src/Blog.php
@@ -10,7 +10,7 @@ use BootPress\Pagination\Component as Pagination;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
 use Spartz\TextFormatter\TextFormatter;
-use URLify; // jbroadway/urlify
+use URLify;
 
 class Blog
 {
