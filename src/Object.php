37fe4c76ac6e38d5918816295be642dd186abff4
Kyle Gadd <kyle.gadd@gmail.com>
2016-10-13 17:00:23 -0800

    Moved page.blog to blog.query

---
+++ src/Object.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BootPress\Blog;
+
+class Object
+{
+    public $properties = array();
+    public $methods = array();
+
+    public function __construct(array $properties = array(), array $methods = array())
+    {
+        $this->properties = $properties;
+        $this->methods = $methods;
+    }
+
+    public function __get($name)
+    {
+        return (isset($this->properties[$name])) ? $this->properties[$name] : null;
+    }
+
+    public function __isset($name)
+    {
+        return (isset($this->methods[$name])) ? false : true;
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (isset($this->methods[$name]) && is_callable($this->methods[$name])) {
+            $result = call_user_func_array($this->methods[$name], $arguments);
+        }
+
+        return (isset($result) && !is_object($result)) ? $result : null;
+    }
+}
