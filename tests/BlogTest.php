074cb67d5ddf313b2a5de361bfd3d1188053f4e4
Kyle Gadd <kyle.gadd@gmail.com>
2016-11-29 17:53:34 -0900

    Update BlogTest.php

--- tests/BlogTest.php
+++ tests/BlogTest.php
@@ -8,7 +8,7 @@ use BootPress\Sitemap\Component as Sitemap;
 use BootPress\Pagination\Component as Pagination;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Yaml\Yaml;
-use Aptoma\Twig\Extension\MarkdownEngine\ParsedownEngine;
+use Aptoma\Twig\Extension\MarkdownEngine\PHPLeagueCommonMarkEngine;
 
 class BlogTest extends \BootPress\HTMLUnit\Component
 {
@@ -1898,7 +1898,7 @@ class BlogTest extends \BootPress\HTMLUnit\Component
         $this->assertEquals('<p>There is no &quot;I&quot; in den<strong>i</strong>al</p>', trim(static::$blog->theme->markdown('There is no "I" in den**i**al')));
         $engine = new \BootPress\Blog\Twig\Markdown(static::$blog->theme);
         $this->assertEquals('Blog\Markdown', $engine->getName());
-        $this->assertNull(static::$blog->theme->markdown(new ParsedownEngine));
+        $this->assertNull(static::$blog->theme->markdown(new PHPLeagueCommonMarkEngine));
     }
 
     public function testThemeAssetAndThisMethods()
